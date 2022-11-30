1ba14877cf9994fa1fe7f3bc7f17fff824dd06fb
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-11-30 16:05:03 +0700

    add class stacks

--- StackUsingLinkedList_128/Program.cs
+++ StackUsingLinkedList_128/Program.cs
@@ -12,4 +12,19 @@ namespace StackUsingLinkedList_CSharp
             next = n;
         }
     }
+    class Stacks
+    {
+        Node top;
+        public Stacks()
+        {
+            top = null;
+        }
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+    }
 }
